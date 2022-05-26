5fe169dbdbc22a45e9c2555d7882f770d0d32f14
duynvh <nguyenvohoangduy@gmail.com>
2022-05-26 08:12:57 +0700

    Implement search a rotated array

---
+++ array/14-search-a-rotated-array.ts
@@ -0,0 +1,33 @@
+// Input: [6, 7, 1, 2, 3, 4, 5]. Target 3
+// Output: 4
+
+function binarySearchRotated(arr: number[], target: number): number {
+    let start = 0;
+    let end = arr.length - 1;
+
+    if (start > end) return -1;
+
+    while (start <= end) {
+        let mid = Math.floor(start + (end - start) / 2);
+
+        if (arr[mid] === target) return mid;
+        
+        if (arr[start] <= arr[mid]) {
+            if (arr[start] <= target && target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (arr[mid] < target && target <= arr[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    
+    return -1;
+}
+
+console.log(binarySearchRotated([4, 5, 6, 1, 2, 3], 6));
