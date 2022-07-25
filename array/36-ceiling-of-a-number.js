aaf5d5ba30c55dda1dfa2605cce163c5fe529f8f
duynvh <nguyenvohoangduy@gmail.com>
2022-07-25 15:14:59 +0700

    Implement exercise 36

---
+++ array/36-ceiling-of-a-number.js
@@ -0,0 +1,28 @@
+// Input: [4, 6, 10], key = 17
+// Output: -1
+// Explanation: There is no number greater than or equal to '17' in the given array.
+// Input: [4, 6, 10], key = -1
+// Output: 0
+// Explanation: The smallest number greater than or equal to '-1' is '4' having index '0'.
+function searchCeilingOfANumber(arr, key) {
+    if (key > arr[arr.length - 1])
+        return -1;
+    var start = 0;
+    var end = arr.length - 1;
+    while (start <= end) {
+        var mid = Math.floor((start + (end - start) / 2));
+        if (key < mid) {
+            end = mid - 1;
+        }
+        else if (key > arr[mid]) {
+            start = mid + 1;
+        }
+        else {
+            return mid;
+        }
+    }
+    return start;
+}
+console.log(searchCeilingOfANumber([4, 6, 10], 17));
+console.log(searchCeilingOfANumber([4, 6, 10], -1));
+console.log(searchCeilingOfANumber([1, 3, 8, 10, 15], 12));
