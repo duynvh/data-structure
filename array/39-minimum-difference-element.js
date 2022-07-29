8c875568c97a2d98058c709e3b954288f7616403
duynvh <nguyenvohoangduy@gmail.com>
2022-07-29 08:00:16 +0700

    Implement exercise 39

---
+++ array/39-minimum-difference-element.js
@@ -0,0 +1,34 @@
+// Input: [4, 6, 10], key = 7
+// Output: 6
+// Explanation: The difference between the key '7' and '6' is minimum than any other number in the array 
+// Input: [1, 3, 8, 10, 15], key = 12
+// Output: 10
+// Input: [4, 6, 10], key = 17
+// Output: 10
+function searchMinDiffElement(arr, key) {
+    if (key < arr[0])
+        return arr[0];
+    if (key > arr[arr.length - 1])
+        return arr[arr.length - 1];
+    var start = 0;
+    var end = arr.length - 1;
+    while (start <= end) {
+        var mid = Math.floor(start + (end - start) / 2);
+        if (key < arr[mid]) {
+            end = mid - 1;
+        }
+        else if (key > arr[mid]) {
+            start = mid + 1;
+        }
+        else {
+            return arr[mid];
+        }
+    }
+    if ((arr[start] - key) < (key - arr[end]))
+        return arr[start];
+    return arr[end];
+}
+console.log(searchMinDiffElement([4, 6, 10], 7));
+console.log(searchMinDiffElement([4, 6, 10], 4));
+console.log(searchMinDiffElement([4, 6, 10], 17));
+console.log(searchMinDiffElement([1, 3, 8, 10, 15], 12));
