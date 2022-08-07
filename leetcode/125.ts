159f05e91f1c8de69ea8ae9dbfd3b5e1817ee72c
duynvh <nguyenvohoangduy@gmail.com>
2022-08-07 17:12:39 +0700

    Implement leetcode 125

---
+++ leetcode/125.ts
@@ -0,0 +1,43 @@
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+
+function isPalindrome(str: string): boolean {
+    let start = 0;
+    let length = str.length;
+    let end = length - 1;
+
+    while (start <= end) {
+        while (start < end && !isAlphaNumeric(str[start])) start++; 
+        while (start < end && !isAlphaNumeric(str[end])) end--;
+
+        if (str[start].toUpperCase() !== str[end].toUpperCase()) return false;
+
+        start++;
+        end--;
+    }
+    return true;
+}
+
+function isAlphaNumeric(char: string): boolean {
+    let code: number = char.charCodeAt(0);
+      if (!(code > 47 && code < 58) && // numeric (0-9)
+          !(code > 64 && code < 91) && // upper alpha (A-Z)
+          !(code > 96 && code < 123)) { // lower alpha (a-z)
+        return false;
+    }
+    return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("race a car"));
+console.log(isPalindrome(" "));
