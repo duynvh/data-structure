41cfa7371f9529185e6281765d4133860ca37377
duynvh <nguyenvohoangduy@gmail.com>
2022-08-09 08:25:20 +0700

    Implement leetcode 1047

---
+++ leetcode/1047.ts
@@ -0,0 +1,22 @@
+// Input: s = "abbaca"
+// Output: "ca"
+
+// Input: s = "azxxzy"
+// Output: "ay"
+
+function removeDuplicates(s: string): string {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (stack.length === 0 || s[i] != stack[stack.length - 1]) {
+            stack.push(s[i]);
+        } else {
+            stack.pop();
+        }
+    }
+
+    if (stack.length === 0) return "";
+    return stack.join("");
+};
+
+console.log(removeDuplicates("abbaca"));
+console.log(removeDuplicates("azxxzy"));
