d538c08ec23b65988b7c3858cb89bf0592bf8e40
duynvh <nguyenvohoangduy@gmail.com>
2022-08-11 08:29:42 +0700

    Implement leetcode 509

---
+++ leetcode/509.js
@@ -0,0 +1,20 @@
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+function fib(n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fib(n - 2) + fib(n - 1);
+}
+;
+console.log(fib(4));
+console.log(fib(3));
+console.log(fib(6));
