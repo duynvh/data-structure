7000c3a676c015cb9b12c430328303c34f1f2c6c
duynvh <nguyenvohoangduy@gmail.com>
2022-10-11 22:32:46 +0700

    Implement power, factorial of recurrsion

---
+++ master-coding-interview/13.go
@@ -0,0 +1,20 @@
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(power(2, 0))
+	fmt.Println(power(2, 4))
+}
+
+func power(base, exponent int) int {
+	if exponent == 0 {
+		return 1
+	}
+
+	return base * power(base, exponent - 1)
+}
