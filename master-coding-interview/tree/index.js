288cc9ad8893ef77a7be785e51e3b5a5ed27f75d
duynvh <nguyenvohoangduy@gmail.com>
2022-11-01 15:16:34 +0700

    Implement bfs, dfs tree

--- master-coding-interview/tree/index.js
+++ master-coding-interview/tree/index.js
@@ -57,15 +57,67 @@ class BinarySearchTree {
 
         return current;
     }
+
+    bfs() {
+        let node = this.root;
+        const data = [];
+        const queue = [];
+
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        return data;
+    }
+
+    dfsPreOrder() {
+        const data = [];
+        function traverse(node) {
+            data.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
+
+    dfsPostOrder() {
+        const data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+        return data;
+    }
+
+    dfsInOrder() {
+        const data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            data.push(node.value);
+            if (node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
 }
 
 let tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
-console.log(tree.find(13));
+console.log(tree.dfsInOrder());
